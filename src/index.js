57a1403a19879c6deda0761c7a5b1bcd7123bde1
Daniel Barnes <dabarnes2b@gmail.com>
2022-02-24 09:56:12 -0800

    initial setup

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const core = require('@actions/core');
+const github = require('actions/github');
+const sodium = require('tweetsodium');
+
+async function run() {
+  try {
+
+  } catch (err) {
+    core.setFailed(err.message);
+  }
+}
+
+run();
