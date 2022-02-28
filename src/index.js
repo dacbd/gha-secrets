8089a2ce4b8ac25063385411c66f045467dfd74c
Daniel Barnes <dabarnes2b@gmail.com>
2022-02-27 16:24:47 -0800

    module typo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core');
-const github = require('actions/github');
+const github = require('@actions/github');
 const sodium = require('tweetsodium');
 
 async function run() {
